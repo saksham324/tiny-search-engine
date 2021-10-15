ba1b4c84a1c25e840de4832bb2663c9c572a231a
Saksham Arora <f0042mm@plank.thayer.dartmouth.edu>
2021-10-15 11:54:07 -0400

    added pagedir

---
+++ common/pagedir.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pagedir.h"
+
+// function prototypes
+bool isValidDirectory(char * dirname);
+bool pageSaver(webpage_t* page, char * dirname, const int id);
+
+
+bool validateDirectory(char * pageDir)
+{
+    // name of the file (.crawler or ./crawler depending on the formatting)
+    char filename[11 + strlen(pageDir)];
+    strcpy(filename, pageDir);
+    char *extension;
+
+    // user can give directory as either pageDir/ or pageDir
+    if (pageDir[strlen(pageDir) - 1] != '/'){
+        extension = "/.crawler";
+        strcat(filename, extension);
+    }
+    else {
+        extension = ".crawler";
+        strcat(filename, extension);
+    } 
+
+    // open the file
+    FILE * fp = fopen(filename, "w");
+
+    // if a valid fp is returned, the directory is valid.
+    if (fp == NULL){
+        fprintf(stderr, "Provide an existing pageDirectory.\n");
+        return false;
+    }
+    else {
+        fclose(fp);
+        return true;
+    }
+}
+
+
+// saves a page in the directory specified by pageDir with a file name of id
+bool pageSaver(webpage_t* page, char * pageDir, const int id)
+{
+    bool status; // have we successfully saved the page?
+    char filename[strlen(pageDir) + 5];
+    strcpy(filename, pageDir);
+    // if the filename does not end with a '/', concatenate it
+    if (pageDir[strlen(pageDir) - 1] != '/') {
+        strcat(filename, "/");
+    }
+
+    char idName[4];
+
+    // converting int id to string  
+    sprintf(idName, "%d", id);
+
+    // concatenating filename and id into an absolute path
+    strcat(filename, idName);
+
+    FILE *fp = fopen(filename, "w");
+
+    if (fp == NULL){
+        fprintf(stderr, "pageSaver: Error saving the webpage.\n");
+        status = false;
+    }
+    // if the file was successfully created, start writing into it.
+    else {
+        fprintf(fp, webpage_getURL(page));
+        fprintf(fp, "\n");
+        fprintf(fp, "%d\n", webpage_getDepth(page));
+        fprintf(fp, webpage_getHTML(page));
+        fclose(fp);
+        status = true;
+    }
+    return status;   
+}
+
+
--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,6 +1,7 @@
 /* 
 - crawler.c
-
+- Lab 4 Submission 
+- Author : Saksham Arora
 */
 
 #include <stdio.h>
@@ -9,110 +10,130 @@
 #include "../libcs50/memory.h"
 #include "../libcs50/bag.h"
 #include "../libcs50/hashtable.h"
+#include "../common/pagedir.h"
 
-char* PAGE_DIRECTORY; 
-webpage_t* pageFetcher(char* seedUrl, const int depth); 
-void pageSaver(webpage_t* page, char* dirName, int id); 
+// function prototypes 
+bool pageFetcher(webpage_t *page); 
+void pageSaver(webpage_t* page, char* dirName, int id);
+void url_delete(void *url);  
 
-int main(const int argc, char *argv[])
-{
-	bag_t *bag = bag_new();
-    hashtable_t *hashtable = hastable_new(50); 
+int main(const int argc, char *argv[]){
 
-
+    // define variables 
+    char *seedUrl; 
+    int maxDepth;
+    char pageDirectory[50];
     char* program = argv[0];  // program name
+    int id = 1; 
+  
 
 	// check input arguments: # of arguments
 	if (argc != 4) {
 		fprintf(stderr, "usage: %s seedURL pageDirectory maxDepth\n", program);
 		return 1; 
 	}
+
 	// check input arguments: URL validity
-	char* seedUrl = malloc(sizeof(argv[1] + 1)); 
-	strcpy(seedUrl, argv[1]); 
-	if (!NormalizeURL(seedUrl)) {
-		fprintf(stderr, "%s cannot be normalized\n", seedUrl);
-		return 2;
-	}
-
-    if (!IsInternalURL(seedUrl)) {
-		fprintf(stderr, "%s is not an internal URL\n", seedUrl);
-		return 3;
-	}
-
-    PAGE_DIRECTORY = argv[3]; 
-
-    webpage_t *seedPage = webpage_new(seedUrl, 0, NULL); 
-    bag_insert(bag, seedPage); 
-    hashtable_insert(hashtable, seedUrl, seedPage); 
-
-    
-    void* bagNext; 
-    while ((bagNext = bag_extract(bag)) != NULL) {
-        webpage_t* page = pageFetcher(bagNext, 1); 
+	if (IsInternalURL(argv[1])) {
+        seedUrl = assertp(count_malloc((strlen(argv[1]) + 1)*sizeof(char)), "seedUrl");
+        strcpy(seedUrl, argv[1]);
     }
+    else {
+        fprintf(stderr, "Invalid seedUrl.\n");
+        return 2;
+    }
+
+    // validating maxDepth
+    maxDepth = atoi(argv[3]);
+    if (maxDepth < 0){
+        fprintf(stderr, "maxDepth has to be > 0.\n");
+        return 3;
+    }
+
+    // validating directory
+    strcpy(pageDirectory, argv[2]);
+    if (!isValidDirectory(argv[2])) {
+        return 4;
+    }
+
+    // define bag with pages to crawl
+    bag_t *pagesToCrawl = bag_new();
+
+    // define hashtable for seen urls
+    hashtable_t *urlSeen = hastable_new(200);
+
+    // define seed page from seedUrl
+    webpage_t *seedPage = webpage_new(seedUrl, 0, NULL); 
+ 
+    // insert seedPage to bag
+    bag_insert(pagesToCrawl, seedPage); 
+
+    // add seedUrl to ht of seen urls
+    hashtable_insert(urlSeen, seedUrl, ""); 
+    
+    // define webpage last extracted from bag
+    webpage_t *lastExtracted; 
+
+    while ((lastExtracted = bag_extract(pagesToCrawl)) != NULL) {
+        if (pageFetcher(lastExtracted)){
+            char dirName[50]; 
+            strcpy(dirName, pageDirectory); 
+            pageSaver(lastExtracted, dirName, id); // save the webpage
+            id++; 
+        } 
+
+        // if depth of webpage is less than max depth continue scanning
+        if (webpage_getDepth(lastExtracted) < maxDepth) {
+            pageScanner(lastExtracted, pagesToCrawl, urlSeen); // scan webpage for more urls and add to bag
+        }
+
+        // delete webpage 
+        webpage_delete(lastExtracted); 
+    }
+
+    // delete bag once we're done
+    bag_delete(pagesToCrawl, webpage_delete); 
+
+    // delete hashtable once we're done 
+    hashtable_delete(urlSeen, url_delete); 
+
 	return 0;
 }
 
-webpage_t* pageFetcher(char* url, const int depth) {
-    // char* url = malloc(sizeof(url+ 1)); 
-    // strcpy(url, url); 
-    if (!NormalizeURL(url)) {
-		fprintf(stderr, "%s cannot be normalized\n", url);
-		return NULL;
-	}
-
-    if (!IsInternalURL(url)) {
-		fprintf(stderr, "%s is not internal\n", url);
-		return NULL;
-	}
-
-    webpage_t *page = webpage_new(url, depth, NULL); 
-    if(!webpage_fetch(page)) {
-        return page; 
-    } else {
-        return NULL; 
-    }
+// pagefetcher function definition
+bool pageFetcher(webpage_t *page) {
+    return webpage_fetch(page);
 }
 
+// pagescanner function definition 
 void pageScanner(webpage_t* page, bag_t *bag, hashtable_t *ht) {
     int pos = 0; 
     char *result; 
 
     while((result = webpage_getNextURL(page, &pos)) != NULL) {
-        webpage_t *page_new = pageFetcher(result, webpage_getDepth(page) + 1); 
-        if (page_new != NULL) {
-            hashtable_insert(ht, result, result); 
-            bag_insert(bag, page_new);
+       if (IsInternalURL(result)){
+            if(hashtable_insert(ht, result, "")){
+                webpage_t *new = webpage_new(result, webpage_getDepth(page) + 1, NULL);
+                bag_insert(bag, new);
+            } else {
+                // free the URL if it wasn't inserted in the hashtable
+                free(result);
+            }
+        } else {
+            // free the URL if it's not internal
+            free(result);
         }
     }
-    free(result); 
 }
 
-void pageSaver(webpage_t* page, char* dirName, int id) {
-    int id_length = snprintf(NULL, 0, '%d', id); 
-    char* idStr = malloc(id_length + 1); 
-    char* filePath = malloc(strlen(dirName) + strlen(idStr) + 2); 
-
-    filePath[0] = '\0'; 
-    strcat(filePath, dirName); 
-    strcat(filePath, "/"); 
-    strcat(filePath, idStr); 
-
-    FILE *fp = fopen(filePath, "w"); 
-    free(idStr); 
-
-    if (fp != NULL){
-        fprintf(fp, "%s\n", webpage_getURL(page));
-        fprintf(fp, "%s\n", webpage_getDepth(page)); 
-        fprintf(fp, "%s\n", webpage_getHTML(page)); 
-    } else {
-        fprintf("Error making file %s\n", filePath); 
+// function to delete url 
+void url_delete(void *url) {
+    if (url != NULL) {
+        url = NULL;
     }
-
-    free(filePath);
-
 }
+
+
 
 
 
